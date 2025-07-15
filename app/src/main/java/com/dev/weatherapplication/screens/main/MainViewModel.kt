dd70c798d882c3150d2b7b10fb50f05f219967c0
Akanbi Rahmon <23046879+Rahmon231@users.noreply.github.com>
2025-07-15 13:50:34 +0100

    feat: added main viewmodel

--- app/src/main/java/com/dev/weatherapplication/screens/main/MainViewModel.kt
+++ app/src/main/java/com/dev/weatherapplication/screens/main/MainViewModel.kt
@@ -1,12 +1,17 @@
 package com.dev.weatherapplication.screens.main
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import com.dev.weatherapplication.data.WeatherState
+import com.dev.weatherapplication.model.Weather
 import com.dev.weatherapplication.repository.WeatherRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor(private val weatherRepository: WeatherRepository) : ViewModel() {
+class MainViewModel @Inject constructor(private val weatherRepository: WeatherRepository)
+    : ViewModel() {
 
+    val weatherState = mutableStateOf<WeatherState<Weather>>(WeatherState.Loading)
 
 }
